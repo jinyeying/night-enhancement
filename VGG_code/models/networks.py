db02e39658e926f5dff24b5d7babc59ba42dd1f0
AuAgCu <36725804+jinyeying@users.noreply.github.com>
2022-07-26 22:29:55 +0800

    Add files via upload

---
+++ VGG_code/models/networks.py
@@ -0,0 +1,92 @@
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+from torch.autograd import Variable
+from torch.nn import init
+import functools, itertools
+import numpy as np
+from util.util import gkern_2d
+import os
+from torchvision import models as visionmodels
+
+class MeanShift(nn.Conv2d):
+    def __init__(self, data_mean, data_std, data_range=1, norm=True):
+        """norm (bool): normalize/denormalize the stats"""
+        c = len(data_mean)
+        super(MeanShift, self).__init__(c, c, kernel_size=1)
+        std = torch.Tensor(data_std)
+        self.weight.data = torch.eye(c).view(c, c, 1, 1)
+        if norm:
+            self.weight.data.div_(std.view(c, 1, 1, 1))
+            self.bias.data = -1 * data_range * torch.Tensor(data_mean)
+            self.bias.data.div_(std)
+        else:
+            self.weight.data.mul_(std.view(c, 1, 1, 1))
+            self.bias.data = data_range * torch.Tensor(data_mean)
+        self.requires_grad = False
+
+class Vgg16ExDark(torch.nn.Module):
+    def __init__(self, load_model=None, requires_grad=False):
+        super(Vgg16ExDark, self).__init__()
+        # Create the model
+        self.vgg_pretrained_features = visionmodels.vgg16(pretrained=True).features
+        if load_model is None:
+            print('Vgg16ExDark needs a pre-trained checkpoint!')
+            raise Exception
+        else:
+            print('Vgg16ExDark initialized with %s'% load_model)
+            model_state_dict = torch.load(load_model)
+            model_dict       = self.vgg_pretrained_features.state_dict()
+            # The checkpoint has keys wuth 'module.features.',
+            # for k, v in model_state_dict.items():
+                # print(k[16:])
+            model_state_dict = {k[16:]: v for k, v in model_state_dict.items() if k[16:] in model_dict}
+            self.vgg_pretrained_features.load_state_dict(model_state_dict)
+        if not requires_grad:
+            for param in self.parameters():
+                param.requires_grad = False
+                # print(param, param.requires_grad)
+
+    def forward(self, X, indices=None):
+        if indices is None:
+            indices = [3, 8, 15, 22] # assuming 0 starting index!
+        out = []
+        #indices = sorted(indices)
+        for i in range(indices[-1]+1):
+            # print(i, self.vgg_pretrained_features[i])
+            X = self.vgg_pretrained_features[i](X)
+            if i in indices:
+                out.append(X)
+        return out
+
+"""
+Tutorials/Helpers
+https://gist.github.com/brucemuller/37906a86526f53ec7f50af4e77d025c9
+https://towardsdatascience.com/pytorch-implementation-of-perceptual-losses-for-real-time-style-transfer-8d608e2e9902
+"""
+class PerceptualLossVgg16ExDark(nn.Module):
+    def __init__(self, vgg=None, load_model=None, gpu_ids=[0], weights=None, indices=None, normalize=True):
+        super(PerceptualLossVgg16ExDark, self).__init__()        
+        if vgg is None:
+            self.vgg = Vgg16ExDark(load_model)
+        else:
+            self.vgg = vgg
+        self.vgg = nn.DataParallel(self.vgg, device_ids=gpu_ids).cuda()
+        self.criterion = nn.L1Loss()
+        self.weights = weights or [1.0, 1.0, 1.0, 1.0]
+        self.indices = indices or [3, 8, 15, 22]
+        if normalize:
+            self.normalize = MeanShift([0.485, 0.456, 0.406], [0.229, 0.224, 0.225], norm=True).cuda()
+        else:
+            self.normalize = None
+
+    def forward(self, x, y):
+        if self.normalize is not None:
+            x = self.normalize(x)
+            y = self.normalize(y)
+        x_vgg, y_vgg = self.vgg(x, self.indices), self.vgg(y, self.indices)
+        loss = 0
+        for i in range(len(x_vgg)):
+            # print(len(x_vgg), x_vgg[0].size(), y_vgg[0].size())
+            loss += self.weights[i] * self.criterion(x_vgg[i], y_vgg[i].detach())
+        return loss
